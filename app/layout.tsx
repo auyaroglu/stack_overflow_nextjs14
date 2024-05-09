e751d2bb2b3ff85fb091502d011d7e51e7997660
auyaroglu <ardauyaroglu@hotmail.com>
2024-05-09 17:59:05 +0300

    add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,42 @@
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import React from "react"
+import { Inter, Space_Grotesk } from "next/font/google"
+import type { Metadata } from "next"
 import "./globals.css"
+
+const inter = Inter({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    variable: "--font-inter",
+})
+
+const spaceGrotesk = Space_Grotesk({
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "600", "700"],
+    variable: "--font-spaceGrotesk",
+})
+
+export const metadata: Metadata = {
+    title: "DevFlow",
+    description: "A community-driven platform for asking and answering questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms,data structures, and more.",
+    icons: {
+        icon: "/assets/images/site-logo.svg",
+    },
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <ClerkProvider>
+        <ClerkProvider
+            appearance={{
+                elements: {
+                    formButtonPrimary: "primary-gradient",
+                    footerActionLink: "primary-text-gradient hover:text-primary-500",
+                },
+            }}
+        >
             <html lang="en">
-                <body>
+                <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+                    <h1 className="h1-bold">This is a text</h1>
                     <header>
                         <SignedOut>
                             <SignInButton />
