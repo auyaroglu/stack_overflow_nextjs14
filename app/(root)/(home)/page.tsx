f29efa027f146158938e3b33fbccd2b746773b3d
auyaroglu <ardauyaroglu@hotmail.com>
2024-05-11 12:06:45 +0300

    implement theme provider

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from "@clerk/nextjs"
 import React from "react"
 
 const Home = () => {
-    return <div>Home</div>
+    return (
+        <div>
+            <UserButton afterSignOutUrl="/" />
+        </div>
+    )
 }
 
 export default Home
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import { Inter, Space_Grotesk } from "next/font/google"
 import type { Metadata } from "next"
 import "./globals.css"
+import { ThemeProvider } from "@/context/ThemeProvider"
 
 const inter = Inter({
     subsets: ["latin"],
@@ -26,28 +27,21 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <ClerkProvider
-            appearance={{
-                elements: {
-                    formButtonPrimary: "primary-gradient",
-                    footerActionLink: "primary-text-gradient hover:text-primary-500",
-                },
-            }}
-        >
-            <html lang="en">
-                <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-                    <h1 className="h1-bold">This is a text</h1>
-                    <header>
-                        <SignedOut>
-                            <SignInButton />
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton />
-                        </SignedIn>
-                    </header>
-                    <main>{children}</main>
-                </body>
-            </html>
-        </ClerkProvider>
+        <html lang="en">
+            <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+                <ClerkProvider
+                    appearance={{
+                        elements: {
+                            formButtonPrimary: "primary-gradient",
+                            footerActionLink: "primary-text-gradient hover:text-primary-500",
+                        },
+                    }}
+                >
+                    <ThemeProvider>
+                        {children}
+                    </ThemeProvider>
+                </ClerkProvider>
+            </body>
+        </html>
     )
 }
