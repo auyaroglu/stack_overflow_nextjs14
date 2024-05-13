e1857e5e4ccbe399f7e9af12cc2209780045668c
auyaroglu <ardauyaroglu@hotmail.com>
2024-05-13 16:37:18 +0300

    Right sidebar created

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from "@/components/shared/LeftSidebar"
+import RightSidebar from "@/components/shared/RightSidebar"
 import Navbar from '@/components/shared/navbar/Navbar'
 import React from 'react'
 
@@ -13,7 +14,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
                         {children}
                     </div>
                 </section>
-                RightSidebar
+                <RightSidebar />
             </div>
 
             Toaster
