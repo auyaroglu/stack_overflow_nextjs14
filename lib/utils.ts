ffe9ddbaf9ec59ba79c7c0857a35abc7d7691d3f
auyaroglu <ardauyaroglu@hotmail.com>
2024-06-06 22:54:08 +0300

    Homepage local searchbar actions has done

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import qs from "query-string"
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
@@ -48,4 +49,45 @@ export const formatAndDivideNumber = (num: number): string => {
 export const getJoinedDate = (date: Date): string => {
     const options: Intl.DateTimeFormatOptions = { month: 'long', year: 'numeric' };
     return date.toLocaleDateString('en-US', options);
+}
+
+interface UrlQueryParams {
+    params: string,
+    key: string,
+    value: string | null
+}
+
+export const formUrlQuery = ({ params, key, value }: UrlQueryParams) => {
+    const currentUrl = qs.parse(params)
+
+    currentUrl[key] = value
+
+    return qs.stringifyUrl(
+        {
+            url: window.location.pathname,
+            query: currentUrl
+        },
+        { skipNull: true }
+    )
+}
+
+interface RemoveUrlQueryParams {
+    params: string,
+    keysToRemove: string[],
+}
+
+export const removeKeysFromQuery = ({ params, keysToRemove }: RemoveUrlQueryParams) => {
+    const currentUrl = qs.parse(params)
+
+    keysToRemove.forEach((key) => {
+        delete currentUrl[key]
+    })
+
+    return qs.stringifyUrl(
+        {
+            url: window.location.pathname,
+            query: currentUrl
+        },
+        { skipNull: true }
+    )
 }
